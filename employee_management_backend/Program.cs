d113e981fe795c44f73413c879cb9d80ae722632
Jeswin Jose <jeswin.j@thinkpalm.com>
2024-10-21 11:49:02 +0530

    added comments on how to call functions related to table

--- employee_management_backend/Program.cs
+++ employee_management_backend/Program.cs
@@ -9,6 +9,32 @@ namespace employee_management_backend
         {
             DataSeeder.SeedData();
             Console.WriteLine("Data seeding complete.");
+// Get an employee by EmpCode            
+// var employee = DataSeeder.GetEmployeeByEmpCode(1);
+// if (employee != null)
+// {
+//     Console.WriteLine($"Employee Found: {employee.EmpName}, Location: {employee.Location}");
+// }
+// else
+// {
+//     Console.WriteLine("Employee not found.");
+// }
+
+
+
+
+// Get all employees
+// var employees = DataSeeder.GetAllEmployees();
+// foreach (var emp in employees)
+// {
+//     Console.WriteLine($"EmpCode: {emp.EmpCode}, Name: {emp.EmpName}, Location: {emp.Location}");
+// }
+
+
+// Check if an employee exists by EmpCode
+// bool exists = DataSeeder.EmployeeExists(1);
+// Console.WriteLine(exists ? "Employee exists." : "Employee does not exist.");
+
         }
     }
 }
